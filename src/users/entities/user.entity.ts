7908333a74ef30734a3c66512629c327db821207
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-12-28 21:51:07 +0100

    add user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,14 @@
-export class User {}
+export class User {
+    Name: string;
+    Email: string;
+    Phone: number;
+    Age: number;
+    Password: string;
+}
+
+
+export function Age(birthDate: Date): number {
+    let date  = new Date()
+    let myAge = date.getDate() - birthDate.getDate()
+    return myAge
+}
