d91370e296ce50df67d01d9f02ab8c342dbb5d4c
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-12-29 04:33:30 +0100

    add module auth

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { AuthService } from './auth/auth.service';
+import { AuthController } from './auth/auth.controller';
 
 @Module({
-  controllers: [UsersController],
-  providers: [UsersService]
+  controllers: [UsersController, AuthController],
+  providers: [UsersService, AuthService]
 })
 export class UsersModule {}
