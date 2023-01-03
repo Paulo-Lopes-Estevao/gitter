0298a8a5cc123812e1ffabc66833c4c47261fcce
Paulo Lopes Estevão <pl1745240@gmail.com>
2023-01-03 23:08:16 +0100

    add method login

--- src/users/auth/auth.controller.ts
+++ src/users/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+    constructor(private authService: AuthService){}
+
+    @Post("login")
+    login(@Body() body){
+        this.authService.login(body.email, body.password)
+    }
+
+}
--- src/users/auth/auth.service.ts
+++ src/users/auth/auth.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    login(email: string, password: string) {
+        console.log(email, password);
+    }
+}
